a1691ebb46dc64340ff12c751f976072fd2e918c
Hannes Schulz <haschulz@microsoft.com>
2017-06-30 16:12:55 +0000

    fix create directories for image feature pickle (was not done for vgg)

--- src/guesswhat/preprocess_data/extract_img_features.py
+++ src/guesswhat/preprocess_data/extract_img_features.py
@@ -138,6 +138,11 @@ with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, allow_soft_placem
 
 if args.network == "vgg":
     print("Dump file...")
+    try:
+        # this isn't done for vgg above, only for resnet
+        os.makedirs(out_dir)
+    except FileExistsError:
+        pass
     pickle_dump(features, os.path.join(out_dir, feature_name))
 
 print("Done!")
